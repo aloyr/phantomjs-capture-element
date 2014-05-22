f7aaf514e29b12ef39e336531154bdd819c677e3
Peter Carrero <peter.carrero@gmail.com>
2014-05-21 22:11:22 -0500

    Add more pages

--- get_filters.bash
+++ get_filters.bash
@@ -6,12 +6,28 @@ function callPhantom() {
   phantomjs test.js $URL $BLOCK $FILE
 }
 
-URL="http://www.hidglobal.com/products/card-printers/fargo"
+URL="http://rs-dev.hidglobal.com/products/card-printers/fargo"
 BLOCK="block-hid-custom-product-filter"
 FILE="$dia-fargo-card-printers"
 callPhantom
 
-URL="http://www.hidglobal.com/product-display/cards-and-credentials"
+URL="http://rs-dev.hidglobal.com/product-display/cards-and-credentials"
 BLOCK="block-views-exp-product-display-page"
 FILE="$dia-cards-and-credentials"
 callPhantom
+
+URL="http://rs-dev.hidglobal.com/products/rfid-tags/identification-technologies"
+BLOCK="block-hid-custom-product-filter"
+FILE="$dia-identification-technologies"
+callPhantom
+
+URL="http://rs-dev.hidglobal.com/partners"
+BLOCK="block-hid-partner-dir-partner-dir-filter"
+FILE="$dia-partners"
+callPhantom
+
+URL="http://rs-dev.hidglobal.com/documents"
+BLOCK="block-hid-document-widgets-doc-filter-selective"
+FILE="$dia-documents"
+callPhantom
+
