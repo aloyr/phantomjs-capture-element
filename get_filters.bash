7a4b1f2ed71db8fb7c0ca61ac2749c13f4e402dc
Peter Carrero <peter.carrero@gmail.com>
2014-05-30 17:03:47 -0500

    Rework call to function

--- get_filters.bash
+++ get_filters.bash
@@ -1,121 +1,85 @@
 #!/bin/bash
 dia="deliverables/`date "+%Y%m%d-%H%M%S-"`"
+urlproden="http://www.hidglobal.com/"
+urlprodpt="http://www.hidglobal.pt/"
+urldeven="http://rs-dev.hidglobal.com/"
+urldevpt="http://rs-dev.hidglobal.pt/"
 mkdir deliverables 2> /dev/null
 
 function callPhantom() {
-  phantomjs idGetter.js $URL $BLOCK $FILE
+  phantomjs idGetter.js $urlproden$URL $BLOCK $dia-chrome-$FILE-prod
+  phantomjs idGetter.js $urlprodpt$URL $BLOCK $dia-chrome-$FILE-prod-pt
+  phantomjs idGetter.js $urldeven$URL $BLOCK $dia-chrome-$FILE-dev
+  phantomjs idGetter.js $urldevpt$URL $BLOCK $dia-chrome-$FILE-dev-pt
 }
 
-URL="http://rs-dev.hidglobal.com/products/card-printers/fargo"
+URL="products/card-printers/fargo"
 BLOCK="block-hid-custom-product-filter"
-FILE="$dia-fargo-card-printers"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/products/card-printers/fargo" 
-FILE="$dia-fargo-card-printers-pt"
+FILE="fargo-card-printers"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/product-display/cards-and-credentials"
+URL="product-display/cards-and-credentials"
 BLOCK="block-views-exp-product-display-page"
-FILE="$dia-cards-and-credentials"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/product-display/cards-and-credentials"
-FILE="$dia-cards-and-credentials-pt"
+FILE="cards-and-credentials"
 callPhantom
 
 
-URL="http://rs-dev.hidglobal.com/products/rfid-tags/identification-technologies"
+URL="products/rfid-tags/identification-technologies"
 BLOCK="block-hid-custom-product-filter"
-FILE="$dia-identification-technologies"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/products/rfid-tags/identification-technologies" 
-FILE="$dia-identification-technologies"
+FILE="identification-technologies"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/partners"
+URL="partners"
 BLOCK="block-hid-partner-dir-partner-dir-filter"
-FILE="$dia-partners"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/partners"
-FILE="$dia-partners-pt"
+FILE="partners"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/documents"
+URL="documents"
 BLOCK="block-hid-document-widgets-doc-filter-selective"
-FILE="$dia-documents"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/documents"
-FILE="$dia-documents-pt"
+FILE="documents"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/drivers"
+URL="drivers"
 BLOCK="block-hid-selective-forms-drivers-custom-search"
-FILE="$dia-drivers"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/drivers"
-FILE="$dia-drivers"
+FILE="drivers"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/partners/genuine-hid-technology"
+URL="partners/genuine-hid-technology"
 BLOCK="block-views-672fc2997a191dea03bac3d4b06a2bf2"
-FILE="$dia-genuine-hid-technology"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/partners/genuine-hid-technology"
-FILE="$dia-genuine-hid-technology-pt"
+FILE="genuine-hid-technology"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/tracking/assets"
+URL="tracking/assets"
 BLOCK="block-hid-download-tracking-widgets-download-tracking-widgets"
-FILE="$dia-assets"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/tracking/assets"
-FILE="$dia-assets-pt"
+FILE="assets"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/videos"
+URL="videos"
 BLOCK="block-views-exp-videos-page"
-FILE="$dia-videos"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/videos"
-FILE="$dia-videos-pt"
+FILE="videos"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/case-studies"
+URL="case-studies"
 BLOCK="block-views-exp-case-studies-page"
-FILE="$dia-case-studies"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/case-studies"
-FILE="$dia-case-studies-pt"
+FILE="case-studies"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/worldwide-sales-tool"
+URL="worldwide-sales-tool"
 BLOCK="block-views-exp-worldwide-sales-tool-page"
-FILE="$dia-worldwide-sales-tool"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/worldwide-sales-tool"
-FILE="$dia-worldwide-sales-tool-pt"
+FILE="worldwide-sales-tool"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/customer-service"
+URL="customer-service"
 BLOCK="block-views-exp-customer-service-tool-page"
-FILE="$dia-customer-service"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/customer-service"
-FILE="$dia-customer-service-pt"
+FILE="customer-service"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/careers"
+URL="careers"
 BLOCK="block-views-exp-careers-page"
-FILE="$dia-careers"
-callPhantom
-URL="http://rs-dev.hidglobal.pt/careers"
-FILE="$dia-careers-pt"
+FILE="careers"
 callPhantom
 
-URL="http://rs-dev.hidglobal.com/products/readers/omnikey"
+URL="products/readers/omnikey"
 BLOCK="block-hid-custom-product-filter"
-FILE="$dia-omnikey"
+FILE="omnikey"
 callPhantom
-URL="http://rs-dev.hidglobal.pt/products/readers/omnikey"
-FILE="$dia-omnikey-pt"
-callPhantom
-
